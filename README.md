42f4588319e0f1b54c979ea27809ed4e94c2410b
qugu2427 <qugu2427@colorado.edu>
2023-10-22 10:52:31 -0600

    added check tests

---
+++ README.md
@@ -0,0 +1,38 @@
+# Go SPF
+A sender policy framework module for go. Based off rules at http://www.open-spf.org/SPF_Record_Syntax/. 100% test coverage.
+
+# Example usage
+```go
+import (
+    "net"
+    "spf"
+)
+
+senderIp := net.ParseIP("0.0.0.0")
+senderDomain := "google.com"
+
+// CheckHost is the only exported function in the module:
+result, err := spf.CheckHost(senderIp, senderDomain)
+
+switch result {
+    case spf.ResultPass:
+        fmt.Println("Passed spf check! Accept mail from sender.")
+    case spf.ResultFail:
+        fmt.Println("Failed spf check! Reject mail from sender.")
+    case spf.ResultSoftFail:
+        fmt.Println("Soft failed spf check! Reject mail from sender or mark as spam.")
+    case spf.ResultNeutral:
+        fmt.Println("Neutral spf check result! Reject mail from sender or mark as spam.")
+    case spf.ResultNone:
+        fmt.Println("This means there was no spf record for the domain. Reject mail from this sender.")
+    case ResultPermError:
+        fmt.Println("This probably means the spf record for domain was invalid. Reject mail from this sender.")
+        fmt.Println(err)
+    case ResultTempError:
+        fmt.Println("This is a legacy thing. Just treat this as a perm error if you don't know.")
+        fmt.Println(err)
+    default:
+        fmt.Println(err)
+}
+```
+
--- checks_test.go
+++ checks_test.go
@@ -23,8 +23,79 @@ func TestCheckExists(t *testing.T) {
 	}
 }
 
-// TODO check ptr
+func TestCheckPtr(t *testing.T) {
+	type set struct {
+		ip     net.IP
+		domain string
+		hit    bool
+	}
+	tests := []set{
+		{
+			net.ParseIP("74.6.143.26"),
+			"yahoo.com",
+			true,
+		},
+		{
+			net.ParseIP("74.6.143.26"),
+			"google.com",
+			false,
+		},
+	}
+	for _, exp := range tests {
+		hit := checkPtr(exp.ip, exp.domain)
+		if hit != exp.hit {
+			t.Fatalf("Expected ptr checkPtr(%s, %s) to result in hit=%t, but got hit=%t", exp.ip, exp.domain, exp.hit, hit)
+		}
+	}
+
+}
+
 // TODO check ip
+func TestCheckIp(t *testing.T) {
+	type set struct {
+		ip    net.IP
+		ipStr string
+		hit   bool
+	}
+	tests := []set{
+		{
+			net.ParseIP("192.168.1.10"),
+			"192.168.1.0/24",
+			true,
+		},
+		{
+			net.ParseIP("10.0.0.5"),
+			"10.0.0.0/8",
+			true,
+		},
+		{
+			net.ParseIP("2001:0db8:85a3:0000:0000:8a2e:0370:7334"),
+			"2001:0db8::/32",
+			true,
+		},
+		{
+			net.ParseIP("2606:4700:3037::6815:6438"),
+			"2606:4700:3037::/48",
+			true,
+		},
+		{
+			net.ParseIP("192.168.1.10"),
+			"10.0.0.0/8",
+			false,
+		},
+		{
+			net.ParseIP("2606:4700:3037::6815:6438"),
+			"1.2.3.4",
+			false,
+		},
+	}
+	for _, exp := range tests {
+		hit := checkIp(exp.ip, exp.ipStr)
+		if exp.hit != hit {
+			t.Fatalf("Got checkIp(%s, %s)=%t, wanted %t", exp.ip, exp.ipStr, exp.hit, hit)
+		}
+	}
+}
 
 func TestCheckWord(t *testing.T) {
 	type set struct {
